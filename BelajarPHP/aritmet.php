6d0a9781210cb172aafb2d24b335eb8335b7679c
Dev Ulla <56549396+dehla@users.noreply.github.com>
2020-10-27 04:38:53 +0700

    Materi ke 3 Aritmatika PHP
    Tugas Pelajaran Ke 3 Aritmatika Dengan PHP
    Jenis Program : PHP
    Terima Kasih

---
+++ BelajarPHP/aritmet.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contoh operator aritmetika</title>
+</head>
+<body>
+
+	<?php
+	print("<b>Pejumlahan dan Pengurangan</b> :<BR>\n");
+	printf("6 + 1 = %d <BR>\n", 6 + 1);
+	printf("6 - 1 = %d <BR>\n", 6 - 1);
+
+	print("<BR><b>Perkalian</b> : <BR>\n");
+	printf("6 * 2 = %d <BR>\n", 6 * 2);
+	printf("6 * 2.5 = %d <BR>\n", 6 * 2.5);
+
+	print("<BR><b>Pembagian</b> : <BR>\n");
+	print("6 / 4 = "); print(6 / 4); print("<BR>\n");
+	print("6 / 4.0 ="); print(6 / 4.0); print("<BR>\n");
+
+	print("6 % 5 ="); print(6 % 5); print("<BR>\n");
+	print("6 % 4 ="); print(6 % 4); print("<BR>\n");
+	print("6 % 3 ="); print(6 % 3); print("<BR>\n");
+	?>
+
+</body>
+</html>
