5a539cf213308f7156fd0aff301c8fcbdee77c00
Dev Ulla <56549396+dehla@users.noreply.github.com>
2020-10-27 04:27:47 +0700

    Menambahkan 1 File Materi Angka.
    Saya Memambahkan File Materi / Latihan Saya menggunakan Php dengan subjek 
    # Membuat Angka Dengan PHP
    # Jenis Program : PHP
    Terima Kasih

---
+++ BelajarPHP/angka.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengulangan dengan for</title>
+</head>
+<body>
+
+	<?php
+	  for ($i=1; $i <=7; $i = $i + 1)
+	  	print("<font size=$i> $i </font><br>");
+
+	  print("yak!");
+	  ?>
+</body>
+</html>	  
