0e4eafc2094c5bc5a3af24dcd525c19c7d7af7c5
Dev Ulla <56549396+dehla@users.noreply.github.com>
2020-10-27 04:48:37 +0700

    Materi Ke 4 Bilangan Matematika PHP
    Tugas Pelajaran Ke 4  Bilangan Matematika Dengan PHP
    Jenis Program : PHP
    
    Terima Kasih

---
+++ BelajarPHP/bilangan.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>menampilkan bilangan 1-25</title>
+</head>
+<body>
+
+	<?php
+	$bilangan = 1;
+	while ($bilangan <= 25)
+	{
+		print("$bilangan <BR>\n");
+
+		$bilangan++;
+	}
+	?>
+
+</body>
+</html>
