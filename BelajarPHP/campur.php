9ff8e0b2fcf6cd934597fb95972277cdbd8d6745
Dev Ulla <56549396+dehla@users.noreply.github.com>
2020-10-27 05:07:24 +0700

    Materi Ke 9 Membuat Campuran File PHP
    Kini Kita Diajarkan Untuk Mencampur Fungsi Php Dengan HTML
    Tugas Materi Ke 9

---
+++ BelajarPHP/campur.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>contoh operasi matematika string dan bilsangan</title>
+</head>
+<body>
+
+	<?php
+	$suhu = "30.8 derajat celcius";
+	print($suhu / 2);
+	?>
+
+</body>
+</html>
